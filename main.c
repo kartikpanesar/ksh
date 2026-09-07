6e49feb5eec3c25be7303b7d147f64ce4e7ed05c
Kartik Panesar <kartikpanesar@gmail.com>
2026-09-07 17:20:19 +0530

    seperate file for builtin commands

--- main.c
+++ main.c
@@ -83,51 +83,22 @@ char **shell_line(){
                         }
                         args = tmp;
                 }
-                args[n_args++] = strdup(buffer);
+                void *tmp = strdup(buffer);
+                if(tmp == NULL){
+                        fprintf(stderr, "Couldn't allocate memory for a argument.\n");
+                        perror("Error: ");
+                        exit(1);
+                }
+                args[n_tokens++] = tmp;
         }
-        args[n] = NULL;
+        args[n_tokens] = NULL;
 
         return args;
 }
 
-int shell_exit(char **args){
-        return 2;
-}
 
 
-int shell_cd(char **args){
-        if(args[1] == NULL){
-                fprintf(stderr, "No arguments found.\n");
-                fprintf(stderr, "usage: cd file_path\n");
-                exit(1);
-        }
-
-        else if(chdir(args[1])==-1){
-                fprintf(stderr, "Couldn't change directory.\n");
-                perror("Error: ");
-                exit(1);
-        }
-        return 1;
-}
-
-
-typedef int (*shell_func) (char **args);
-
-int builtin_run(char **args){
-        int exit_flag = 0;
-        char *builtin_commands[] = {"exit", "cd"};
-        int cmds = 2;
-        shell_func cmd_funcs[] = {&shell_exit, &shell_cd};
-
-        for(int i=0; i<cmds; i++){
-                if(strcmp(builtin_commands[i], args[0])==0){
-                        exit_flag = cmd_funcs[i](args);
-                }
-        }
-        return exit_flag;
-}
-
-int shell_run(char **args){
+int shell_execute(char **args){
         
         pid_t p = fork();
 
